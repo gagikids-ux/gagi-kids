5952165cb2725c473661c034862d87f165d01cde
Gustavo <guga.grjc@gmail.com>
2026-05-28 23:54:13 -0300

    debug: catch init errors in serverless function

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
-import app from '../server.js'
-export default app
+let handler
+
+try {
+  const mod = await import('../server.js')
+  handler = mod.default
+} catch (err) {
+  handler = (req, res) => res.status(500).json({
+    error: err.message,
+    name: err.constructor?.name,
+    stack: err.stack?.split('\n').slice(0, 6)
+  })
+}
+
+export default handler
