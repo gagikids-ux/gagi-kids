70f6fb3b08caef4b154752ba99e788505b242aac
Gustavo <guga.grjc@gmail.com>
2026-05-28 23:55:05 -0300

    fix: use dynamic import in serverless entry to fix Vercel bundling

--- api/index.js
+++ api/index.js
@@ -1,14 +1,2 @@
-let handler
-
-try {
-  const mod = await import('../server.js')
-  handler = mod.default
-} catch (err) {
-  handler = (req, res) => res.status(500).json({
-    error: err.message,
-    name: err.constructor?.name,
-    stack: err.stack?.split('\n').slice(0, 6)
-  })
-}
-
-export default handler
+const { default: app } = await import('../server.js')
+export default app
